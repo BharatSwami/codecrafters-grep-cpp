48e21dcc98b8b497b5afa43ffeda4c8e3495c942
Bharat Swami <bharatswami1299@gmail.com>
2024-04-22 07:06:16 +0530

    stage 6 part 2

--- src/Server.cpp
+++ src/Server.cpp
@@ -16,10 +16,11 @@ bool matchalphanumeric(const std::string& input_line){
     }
     return false;
 }
-bool positiveMatchGroup(const std::string& input_line, const std::string& pattern){
+bool positiveMatchGroup(const std::string& input_line, const std::string& pattern,int start,int end){
     std::stack<char> s;
     std::stack<std::pair<char,char>> s_pair;
-    int idx = 1, patternSize = pattern.length();
+    if(end>=pattern.size()) end = pattern.size();
+    int idx = start, patternSize = end;
     while(idx<patternSize){
         if(idx != patternSize-1 && pattern[idx] == '-'){
             idx++;
@@ -52,10 +53,11 @@ bool positiveMatchGroup(const std::string& input_line, const std::string& patter
     return false;
 
 }
-bool negitiveMatchGroup(const std::string& input_line, const std::string& pattern){
+bool negitiveMatchGroup(const std::string& input_line, const std::string& pattern,int start,int end){
     std::stack<char> s;
     std::stack<std::pair<char,char>> s_pair;
-    int idx = 2, patternSize = pattern.length();
+    if(end>=pattern.length()) end=pattern.length();
+    int idx = start+1, patternSize = end;
     while(idx<patternSize-1){
         if(idx != patternSize-1 && pattern[idx] == '-'){
             idx++;
@@ -88,6 +90,61 @@ bool negitiveMatchGroup(const std::string& input_line, const std::string& patter
     return true;
 
 }
+
+bool match(const std::string& input_line, const std::string& pattern){
+    int i = 0;
+    while(i<input_line.size()){
+        int j = 0;
+        int temp = i;
+        while(j<pattern.size() && temp < input_line.size()){
+            if(pattern[j] == '\\'){
+                j++;
+                if(j<pattern.size()){
+                    if(pattern[j] == 'd'){
+                        if(!isdigit(input_line[temp])){
+                            break;
+                        }
+                        else temp++;
+                    }
+                    else if(pattern[j] == 'w'){
+                        if(!isalnum(input_line[temp])){
+                            break;
+                        }
+                        else temp++;
+                    }
+                    else if(pattern[j] == '[' ){
+                        int start=j;
+                        while(j<pattern.size()){
+                            if(pattern[j] != ']') j++;
+                        }
+                        if(pattern[j] == '^'){
+                            return negitiveMatchGroup(input_line,pattern,start,j+1);
+                        }
+                        else{
+                            return positiveMatchGroup(input_line,pattern,start,j+1);
+                        }
+
+                    }
+                }
+                else{
+                    break;
+                }
+            }
+            else{
+                if(input_line[temp] != pattern[j]){
+                    break;
+                }
+                else temp++;
+            }
+            j++;
+        }
+        if(j == pattern.size()) return true;
+        i++;
+    }
+    
+    return false;
+}
+
 bool match_pattern(const std::string& input_line, const std::string& pattern) {
     if (pattern.length() == 1) {
         return input_line.find(pattern) != std::string::npos;
@@ -108,12 +165,15 @@ bool match_pattern(const std::string& input_line, const std::string& pattern) {
     }
     else if(pattern[0] == '[' && pattern[pattern.length()-1] == ']'){
         if(pattern[1] == '^'){
-            return negitiveMatchGroup(input_line,pattern);
+            return negitiveMatchGroup(input_line,pattern,0,pattern.size());
         }
         else{
-            return positiveMatchGroup(input_line,pattern);
+            return positiveMatchGroup(input_line,pattern,0,pattern.size());
         }
 
+    }
+    else if(pattern.length() > 1){
+        return match(input_line,pattern);
     }
     else {
         throw std::runtime_error("Unhandled pattern " + pattern);
